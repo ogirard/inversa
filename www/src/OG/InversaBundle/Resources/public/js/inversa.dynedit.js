db0323434837a1bfefc8abf849f61f93c7e364f5
Olivier Girard <o.girard@gmx.ch>
2012-03-26 08:10:03 +0200

    table -> div

--- www/src/OG/InversaBundle/Resources/public/js/inversa.dynedit.js
+++ www/src/OG/InversaBundle/Resources/public/js/inversa.dynedit.js
@@ -1,4 +1,4 @@
-function dynedit(initCount, addLinkId, listId) {
+function DynamicEdit(initCount, addLinkId, listId, headerLabel) {
 	var count = initCount;
 	var $addLink = $(addLinkId);
 	var $list = $(listId);
@@ -15,11 +15,11 @@ function dynedit(initCount, addLinkId, listId) {
 	};
 
 	function PrepareEmbeddedForm() {
-
+		$list.css('margin-left', '40px');
 	}
 
 	function MoveAddLink() {
-
+		$addLink.insertBefore($list);
 	}
 
 	function RegisterDynAdd() {
@@ -32,10 +32,20 @@ function dynedit(initCount, addLinkId, listId) {
 			// replace the "$$name$$" with unique id
 			$itemFormDiv = $(newWidget.replace(/\$\$name\$\$/g, count));
 			count++;
+			var $headerLabel = $($('label', $itemFormDiv)[0]); 
+			$headerLabel.text(headerLabel + ' ' + count);
+			$headerLabel.addClass('headerLabel');
+			
 
 			TransformEmbeddedEntry($itemFormDiv);
 			// create a new list element and add it to our list
 			$itemFormDiv.appendTo($list);
+			$itemFormDiv.addClass('nestedItem');
+			$removeLink = $('<a href="#">Entfernen</a>');
+			$removeLink.click(function() {
+				$list.remove($itemFormDiv);
+			});
+			$removeLink.appendTo($itemFormDiv);
 			return false;
 		});
 	}
