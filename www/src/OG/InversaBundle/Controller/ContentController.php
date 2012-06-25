21923896a70566784992ab279244bf8f90863431
Olivier Girard <o.girard@gmx.ch>
2012-06-25 23:25:07 +0200

    location details added

--- www/src/OG/InversaBundle/Controller/ContentController.php
+++ www/src/OG/InversaBundle/Controller/ContentController.php
@@ -12,262 +12,243 @@ use Symfony\Component\Validator\Constraints\MinLength;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Collection;
 
-class ContentController extends Controller {
-	/**
-	 *
-	 * @Route("/", name="_content_home")
-	 */
-	public function homeAction() {
-		return $this
-				->render('OGInversaBundle:Content:index.html.twig',
-						array('name' => 'home'));
-	}
+class ContentController extends Controller
+{
+    /**
+     *
+     * @Route("/", name="_content_home")
+     */
+    public function homeAction()
+    {
+        return $this->render('OGInversaBundle:Content:index.html.twig', array('name' => 'home'));
+    }
 
-	/**
-	 *
-	 * @Route("/inversa", name="_content_inversa")
-	 */
-	public function inversaAction() {
-		return $this
-				->render('OGInversaBundle:Content:inversa.html.twig',
-						array('name' => 'inversa'));
-	}
+    /**
+     *
+     * @Route("/inversa", name="_content_inversa")
+     */
+    public function inversaAction()
+    {
+        return $this->render('OGInversaBundle:Content:inversa.html.twig', array('name' => 'inversa'));
+    }
 
-	/**
-	 *
-	 * @Route("/inversa/panflute", name="_content_inversa_panflute")
-	 */
-	public function panfluteAction() {
-		return $this
-				->render('OGInversaBundle:Content:panflute.html.twig',
-						array('name' => 'panflute'));
-	}
+    /**
+     *
+     * @Route("/inversa/panflute", name="_content_inversa_panflute")
+     */
+    public function panfluteAction()
+    {
+        return $this->render('OGInversaBundle:Content:panflute.html.twig', array('name' => 'panflute'));
+    }
 
-	/**
-	 *
-	 * @Route("/inversa/violin", name="_content_inversa_violin")
-	 */
-	public function violinAction() {
-		return $this
-				->render('OGInversaBundle:Content:violin.html.twig',
-						array('name' => 'violin'));
-	}
+    /**
+     *
+     * @Route("/inversa/violin", name="_content_inversa_violin")
+     */
+    public function violinAction()
+    {
+        return $this->render('OGInversaBundle:Content:violin.html.twig', array('name' => 'violin'));
+    }
 
-	/**
-	 *
-	 * @Route("/inversa/flute", name="_content_inversa_flute")
-	 */
-	public function fluteAction() {
-		return $this
-				->render('OGInversaBundle:Content:flute.html.twig',
-						array('name' => 'flute'));
-	}
+    /**
+     *
+     * @Route("/inversa/flute", name="_content_inversa_flute")
+     */
+    public function fluteAction()
+    {
+        return $this->render('OGInversaBundle:Content:flute.html.twig', array('name' => 'flute'));
+    }
 
-	/**
-	 *
-	 * @Route("/inversa/organ", name="_content_inversa_organ")
-	 */
-	public function organAction() {
-		return $this
-				->render('OGInversaBundle:Content:organ.html.twig',
-						array('name' => 'organ'));
-	}
+    /**
+     *
+     * @Route("/inversa/organ", name="_content_inversa_organ")
+     */
+    public function organAction()
+    {
+        return $this->render('OGInversaBundle:Content:organ.html.twig', array('name' => 'organ'));
+    }
 
-	/**
-	 *
-	 * @Route("/agenda", name="_content_agenda")
-	 */
-	public function agendaAction() {
-		$em = $this->getDoctrine()->getEntityManager();
-		$archiveDate = mktime(0, 0, 0, date("m") - 1, date("d"), date("Y"));
-		$query = $em->getRepository('OGInversaBundle:AgendaItem')
-				->createQueryBuilder('a')->where('a.eventdate > :archivedate')
-				->setParameter('archivedate', date('Y-m-d h:i:s', $archiveDate))
-				->orderBy('a.eventdate', 'ASC')->getQuery();
-		$entities = $query->getResult();
-		$nextEntity = -1;
-		$now = date_create("now");
-		foreach ($entities as $i => $entity) {
-			$daysInFuture = date_diff($entity->getEventDate(), $now)->d;
-			if (($daysInFuture == 0 || $entity->getEventDate() > $now) && $nextEntity == -1) {
-				$nextEntity = $i;
-			}
-		}
+    /**
+     *
+     * @Route("/agenda", name="_content_agenda")
+     */
+    public function agendaAction()
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $archiveDate = mktime(0, 0, 0, date("m") - 1, date("d"), date("Y"));
+        $query = $em->getRepository('OGInversaBundle:AgendaItem')->createQueryBuilder('a')->where('a.eventdate > :archivedate')
+                ->setParameter('archivedate', date('Y-m-d h:i:s', $archiveDate))->orderBy('a.eventdate', 'ASC')->getQuery();
+        $entities = $query->getResult();
+        $nextEntity = -1;
+        $now = date_create("now");
+        foreach ($entities as $i => $entity) {
+            $daysInFuture = date_diff($entity->getEventDate(), $now)->d;
+            if (($daysInFuture == 0 || $entity->getEventDate() > $now) && $nextEntity == -1) {
+                $nextEntity = $i;
+            }
+        }
 
-		$logger = $this->get('logger');
-		$logger->info('$$MY: ' . $nextEntity);
-		
-		// move next agenda item in the future to first position
-		if ($nextEntity >= 0) {
-			$e = $entities[$nextEntity];
-			unset($entities[$nextEntity]);
-			array_unshift($entities, $e);
-		}
+        $logger = $this->get('logger');
+        $logger->info('$$MY: ' . $nextEntity);
 
-		return $this
-				->render('OGInversaBundle:Content:agendacurrent.html.twig',
-						array('name' => 'agendacurrent',
-								'entities' => $entities));
-	}
+        // move next agenda item in the future to first position
+        if ($nextEntity >= 0) {
+            $e = $entities[$nextEntity];
+            unset($entities[$nextEntity]);
+            array_unshift($entities, $e);
+        }
 
-	/**
-	 *
-	 * @Route("/agenda/archive", name="_content_agenda_archive")
-	 */
-	public function agendaarchiveAction() {
-		$em = $this->getDoctrine()->getEntityManager();
-		$archiveDate = mktime(0, 0, 0, date("m") - 1, date("d"), date("Y"));
-		$query = $em->getRepository('OGInversaBundle:AgendaItem')
-				->createQueryBuilder('a')->where('a.eventdate <= :archivedate')
-				->setParameter('archivedate', date('Y-m-d h:i:s', $archiveDate))
-				->orderBy('a.eventdate', 'DESC')->getQuery();
-		$entities = $query->getResult();
+        return $this
+                ->render('OGInversaBundle:Content:agendacurrent.html.twig',
+                        array('name' => 'agendacurrent', 'entities' => $entities));
+    }
 
-		return $this
-				->render('OGInversaBundle:Content:agendaarchive.html.twig',
-						array('name' => 'agendaarchive',
-								'entities' => $entities));
-	}
+    /**
+     *
+     * @Route("/agenda/archive", name="_content_agenda_archive")
+     */
+    public function agendaarchiveAction()
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $archiveDate = mktime(0, 0, 0, date("m") - 1, date("d"), date("Y"));
+        $query = $em->getRepository('OGInversaBundle:AgendaItem')->createQueryBuilder('a')->where('a.eventdate <= :archivedate')
+                ->setParameter('archivedate', date('Y-m-d h:i:s', $archiveDate))->orderBy('a.eventdate', 'DESC')->getQuery();
+        $entities = $query->getResult();
 
-	/**
-	 *
-	 * @Route("/projects", name="_content_projects")
-	 */
-	public function projectsAction() {
-		return $this
-				->render('OGInversaBundle:Content:projects.html.twig',
-						array('name' => 'projects'));
-	}
+        return $this
+                ->render('OGInversaBundle:Content:agendaarchive.html.twig',
+                        array('name' => 'agendaarchive', 'entities' => $entities));
+    }
 
-	/**
-	 *
-	 * @Route("/media", name="_content_media")
-	 */
-	public function mediaAction() {
-		return $this
-				->render('OGInversaBundle:Content:audiosamples.html.twig',
-						array('name' => 'examples'));
-	}
+    /**
+     *
+     * @Route("/projects", name="_content_projects")
+     */
+    public function projectsAction()
+    {
+        return $this->render('OGInversaBundle:Content:projects.html.twig', array('name' => 'projects'));
+    }
 
-	/**
-	 *
-	 * @Route("/media/videos", name="_content_media_videos")
-	 */
-	public function mediaVideosAction() {
-		return $this
-				->render('OGInversaBundle:Content:videos.html.twig',
-						array('name' => 'videos'));
-	}
+    /**
+     *
+     * @Route("/media", name="_content_media")
+     */
+    public function mediaAction()
+    {
+        return $this->render('OGInversaBundle:Content:audiosamples.html.twig', array('name' => 'examples'));
+    }
 
-	/**
-	 *
-	 * @Route("/media/press", name="_content_media_press")
-	 */
-	public function mediaPressAction() {
-		return $this
-				->render('OGInversaBundle:Content:press.html.twig',
-						array('name' => 'press'));
-	}
+    /**
+     *
+     * @Route("/media/videos", name="_content_media_videos")
+     */
+    public function mediaVideosAction()
+    {
+        return $this->render('OGInversaBundle:Content:videos.html.twig', array('name' => 'videos'));
+    }
 
-	/**
-	 *
-	 * @Route("/media/downloads", name="_content_media_downloads")
-	 */
-	public function mediaSamplesAction() {
-		return $this
-				->render('OGInversaBundle:Content:downloads.html.twig',
-						array('name' => 'downloads'));
-	}
+    /**
+     *
+     * @Route("/media/press", name="_content_media_press")
+     */
+    public function mediaPressAction()
+    {
+        return $this->render('OGInversaBundle:Content:press.html.twig', array('name' => 'press'));
+    }
 
-	/**
-	 *
-	 * @Route("/gallery", name="_content_gallery")
-	 */
-	public function galleryAction() {
-		return $this
-				->render('OGInversaBundle:Content:galleries.html.twig',
-						array('name' => 'gallery'));
-	}
+    /**
+     *
+     * @Route("/media/downloads", name="_content_media_downloads")
+     */
+    public function mediaSamplesAction()
+    {
+        return $this->render('OGInversaBundle:Content:downloads.html.twig', array('name' => 'downloads'));
+    }
 
-	/**
-	 *
-	 * @Route("/cds", name="_content_cds")
-	 */
-	public function cdsAction() {
-		return $this
-				->render('OGInversaBundle:Content:cds.html.twig',
-						array('name' => 'cds'));
-	}
+    /**
+     *
+     * @Route("/gallery", name="_content_gallery")
+     */
+    public function galleryAction()
+    {
+        return $this->render('OGInversaBundle:Content:galleries.html.twig', array('name' => 'gallery'));
+    }
 
-	/**
-	 *
-	 * @Route("/contact", name="_content_contact")
-	 */
-	public function contactAction() {
-		$request = $this->getRequest();
-		$mailDataValidation = new Collection(
-				array(
-						'name' => array(
-								new NotBlank(
-										array(
-												'message' => 'Bitte geben Sie Ihren Namen ein')),
-								new MinLength(
-										array('limit' => 3,
-												'message' => 'Bitte geben Sie Ihren Namen ein'))),
-						'email' => array(
-								new NotBlank(
-										array(
-												'message' => 'Bitte geben Sie Ihre E-Mail ein')),
-								new Email(
-										array(
-												'message' => 'Bitte geben Sie eine gültige E-Mail ein'))),
-						'message' => new NotBlank(
-								array(
-										'message' => 'Bitte geben Sie eine Nachricht ein'))));
+    /**
+     *
+     * @Route("/cds", name="_content_cds")
+     */
+    public function cdsAction()
+    {
+        return $this->render('OGInversaBundle:Content:cds.html.twig', array('name' => 'cds'));
+    }
 
-		$mailData = array();
-		$form = $this
-				->createFormBuilder($mailData,
-						array('validation_constraint' => $mailDataValidation))
-				->add('name', 'text',
-						array('required' => false, 'label' => 'Name'))
-				->add('email', 'email',
-						array('required' => false, 'label' => 'E-Mail'))
-				->add('message', 'textarea',
-						array('required' => false, 'label' => 'Nachricht'))
-				->add('captcha', 'captcha',
-						array('width' => '100', 'height' => '32',
-								'required' => false))->getForm();
+    /**
+     *
+     * @Route("/contact", name="_content_contact")
+     */
+    public function contactAction()
+    {
+        $request = $this->getRequest();
+        $mailDataValidation = new Collection(
+                array(
+                        'name' => array(
+                                new NotBlank(array('message' => 'Bitte geben Sie Ihren Namen ein')),
+                                new MinLength(
+                                        array('limit' => 3, 'message' => 'Bitte geben Sie Ihren Namen ein'))),
+                        'email' => array(
+                                new NotBlank(array('message' => 'Bitte geben Sie Ihre E-Mail ein')),
+                                new Email(
+                                        array('message' => 'Bitte geben Sie eine gültige E-Mail ein'))),
+                        'message' => new NotBlank(array('message' => 'Bitte geben Sie eine Nachricht ein'))));
 
-		if ($request->getMethod() == 'POST') {
-			$form->bindRequest($request);
+        $mailData = array();
+        $form = $this->createFormBuilder($mailData, array('validation_constraint' => $mailDataValidation))
+                ->add('name', 'text', array('required' => false, 'label' => 'Name'))
+                ->add('email', 'email', array('required' => false, 'label' => 'E-Mail'))
+                ->add('message', 'textarea', array('required' => false, 'label' => 'Nachricht'))
+                ->add('captcha', 'captcha', array('width' => '100', 'height' => '32', 'required' => false))->getForm();
 
-			if ($form->isValid()) {
+        if ($request->getMethod() == 'POST') {
+            $form->bindRequest($request);
 
-				// data is an array with "name", "email", and "message" keys
-				$data = $form->getData();
-				$message = \Swift_Message::newInstance()
-						->setSubject('Kontakt www.ensemble-inversa.ch')
-						->setFrom($data['email'])
-						->setTo('info@ensemble-inversa.ch')
-						->setBody(
-								$this
-										->renderView(
-												'OGInversaBundle:Content:email.txt.twig',
-												$data));
+            if ($form->isValid()) {
 
-				$this->get('mailer')->send($message);
-				return $this
-						->render(
-								'OGInversaBundle:Content:mailconfirm.html.twig',
-								array('name' => 'contact',
-										'email' => $data['email']));
-			}
-		}
+                // data is an array with "name", "email", and "message" keys
+                $data = $form->getData();
+                $message = \Swift_Message::newInstance()->setSubject('Kontakt www.ensemble-inversa.ch')->setFrom($data['email'])
+                        ->setTo('info@ensemble-inversa.ch')
+                        ->setBody(
+                                $this
+                                        ->renderView('OGInversaBundle:Content:email.txt.twig', $data));
 
-		return $this
-				->render('OGInversaBundle:Content:contact.html.twig',
-						array('name' => 'contact',
-								'form' => $form->createView()));
-	}
+                $this->get('mailer')->send($message);
+                return $this
+                        ->render('OGInversaBundle:Content:mailconfirm.html.twig',
+                                array('name' => 'contact', 'email' => $data['email']));
+            }
+        }
+
+        return $this
+                ->render('OGInversaBundle:Content:contact.html.twig',
+                        array('name' => 'contact', 'form' => $form->createView()));
+    }
+
+    /**
+     * Finds and displays a Location entity.
+     *
+     * @Route("/{name}/location/{id}/show", name="_content_location_show")
+     */
+    public function showAction($name, $id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entity = $em->getRepository('OGInversaBundle:Location')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Location entity.');
+        }
+
+        return $this->render('OGInversaBundle:Content:location.html.twig', array('name' => $name, 'entity' => $entity));
+    }
 }
