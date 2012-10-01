30f3a099f09fcf0f07c7a1ef02da74441713f52e
Olivier Girard <o.girard@gmx.ch>
2012-10-01 22:08:43 +0200

    location handling improved (don't delete locations in use!)

--- www/src/OG/InversaBundle/Controller/LocationController.php
+++ www/src/OG/InversaBundle/Controller/LocationController.php
@@ -16,168 +16,188 @@ use OG\InversaBundle\Form\LocationType;
  */
 class LocationController extends Controller
 {
-    /**
-     * Lists all Location entities.
-     *
-     * @Route("/", name="admin_location")
-     * @Template()
-     */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()->getEntityManager();
+  /**
+   * Lists all Location entities.
+   *
+   * @Route("/", name="admin_location")
+   * @Template()
+   */
+  public function indexAction()
+  {
+    $em = $this->getDoctrine()->getEntityManager();
 
-        $entities = $em->getRepository('OGInversaBundle:Location')->findAll();
+    $entities = $em->getRepository('OGInversaBundle:Location')->findAll();
 
-        return array('entities' => $entities);
+    return array('entities' => $entities);
+  }
+
+  /**
+   * Finds and displays a Location entity.
+   *
+   * @Route("/{id}/show", name="admin_location_show")
+   * @Template()
+   */
+  public function showAction($id)
+  {
+    $em = $this->getDoctrine()->getEntityManager();
+
+    $entity = $em->getRepository('OGInversaBundle:Location')->find($id);
+
+    if (!$entity) {
+      throw $this->createNotFoundException('Unable to find Location entity.');
     }
 
-    /**
-     * Finds and displays a Location entity.
-     *
-     * @Route("/{id}/show", name="admin_location_show")
-     * @Template()
-     */
-    public function showAction($id)
-    {
-        $em = $this->getDoctrine()->getEntityManager();
+    $deleteForm = $this->createDeleteForm($id);
 
-        $entity = $em->getRepository('OGInversaBundle:Location')->find($id);
+    return array('entity' => $entity, 'delete_form' => $deleteForm->createView(),);
+  }
 
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Location entity.');
-        }
+  /**
+   * Displays a form to create a new Location entity.
+   *
+   * @Route("/new", name="admin_location_new")
+   * @Template()
+   */
+  public function newAction()
+  {
+    $entity = new Location();
+    $form = $this->createForm(new LocationType(), $entity);
 
-        $deleteForm = $this->createDeleteForm($id);
+    return array('entity' => $entity, 'form' => $form->createView());
+  }
 
-        return array('entity' => $entity, 'delete_form' => $deleteForm->createView(),);
+  /**
+   * Creates a new Location entity.
+   *
+   * @Route("/create", name="admin_location_create")
+   * @Method("post")
+   * @Template("OGInversaBundle:Location:new.html.twig")
+   */
+  public function createAction()
+  {
+    $entity = new Location();
+    $request = $this->getRequest();
+    $form = $this->createForm(new LocationType(), $entity);
+    $form->bindRequest($request);
+
+    if ($form->isValid()) {
+      $em = $this->getDoctrine()->getEntityManager();
+      $em->persist($entity);
+      $em->flush();
+
+      return $this->redirect($this->generateUrl('admin_location_show', array('id' => $entity->getId())));
+
     }
 
-    /**
-     * Displays a form to create a new Location entity.
-     *
-     * @Route("/new", name="admin_location_new")
-     * @Template()
-     */
-    public function newAction()
-    {
-        $entity = new Location();
-        $form = $this->createForm(new LocationType(), $entity);
+    return array('entity' => $entity, 'form' => $form->createView());
+  }
 
-        return array('entity' => $entity, 'form' => $form->createView());
+  /**
+   * Displays a form to edit an existing Location entity.
+   *
+   * @Route("/{id}/edit", name="admin_location_edit")
+   * @Template()
+   */
+  public function editAction($id)
+  {
+    $em = $this->getDoctrine()->getEntityManager();
+
+    $entity = $em->getRepository('OGInversaBundle:Location')->find($id);
+
+    if (!$entity) {
+      throw $this->createNotFoundException('Unable to find Location entity.');
     }
 
-    /**
-     * Creates a new Location entity.
-     *
-     * @Route("/create", name="admin_location_create")
-     * @Method("post")
-     * @Template("OGInversaBundle:Location:new.html.twig")
-     */
-    public function createAction()
-    {
-        $entity = new Location();
-        $request = $this->getRequest();
-        $form = $this->createForm(new LocationType(), $entity);
-        $form->bindRequest($request);
+    $editForm = $this->createForm(new LocationType(), $entity);
+    $deleteForm = $this->createDeleteForm($id);
 
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getEntityManager();
-            $em->persist($entity);
-            $em->flush();
+    return array('entity' => $entity, 'edit_form' => $editForm->createView(), 'delete_form' => $deleteForm->createView(),
+        'allowdelete' => !$this->hasDependencies($id));
+  }
 
-            return $this->redirect($this->generateUrl('admin_location_show', array('id' => $entity->getId())));
+  /**
+   * Edits an existing Location entity.
+   *
+   * @Route("/{id}/update", name="admin_location_update")
+   * @Method("post")
+   * @Template("OGInversaBundle:Location:edit.html.twig")
+   */
+  public function updateAction($id)
+  {
+    $em = $this->getDoctrine()->getEntityManager();
 
-        }
+    $entity = $em->getRepository('OGInversaBundle:Location')->find($id);
 
-        return array('entity' => $entity, 'form' => $form->createView());
+    if (!$entity) {
+      throw $this->createNotFoundException('Unable to find Location entity.');
     }
 
-    /**
-     * Displays a form to edit an existing Location entity.
-     *
-     * @Route("/{id}/edit", name="admin_location_edit")
-     * @Template()
-     */
-    public function editAction($id)
-    {
-        $em = $this->getDoctrine()->getEntityManager();
+    $editForm = $this->createForm(new LocationType(), $entity);
+    $deleteForm = $this->createDeleteForm($id);
 
-        $entity = $em->getRepository('OGInversaBundle:Location')->find($id);
+    $request = $this->getRequest();
 
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Location entity.');
-        }
+    $editForm->bindRequest($request);
 
-        $editForm = $this->createForm(new LocationType(), $entity);
-        $deleteForm = $this->createDeleteForm($id);
+    if ($editForm->isValid()) {
+      $em->persist($entity);
+      $em->flush();
 
-        return array('entity' => $entity, 'edit_form' => $editForm->createView(), 'delete_form' => $deleteForm->createView(),);
+      return $this->redirect($this->generateUrl('admin_location_edit', array('id' => $id)));
     }
 
-    /**
-     * Edits an existing Location entity.
-     *
-     * @Route("/{id}/update", name="admin_location_update")
-     * @Method("post")
-     * @Template("OGInversaBundle:Location:edit.html.twig")
-     */
-    public function updateAction($id)
-    {
-        $em = $this->getDoctrine()->getEntityManager();
+    return array('entity' => $entity, 'edit_form' => $editForm->createView(), 'delete_form' => $deleteForm->createView(),);
+  }
 
-        $entity = $em->getRepository('OGInversaBundle:Location')->find($id);
+  function hasDependencies($id)
+  {
+    $em = $this->getDoctrine()->getEntityManager();
+    $entity = $em->getRepository('OGInversaBundle:Location')->find($id);
 
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Location entity.');
-        }
+    $dependencies = 0;
+    $queryAgenda = $em->getRepository('OGInversaBundle:AgendaItem')->createQueryBuilder('e')->where('e.location = :location_id')
+        ->setParameter('location_id', $entity->getId())->getQuery();
+    $dependencies += count($queryAgenda->getResult());
+    $queryProject = $em->getRepository('OGInversaBundle:ProjectItem')->createQueryBuilder('e')->where('e.location = :location_id')
+        ->setParameter('location_id', $entity->getId())->getQuery();
+    $dependencies += count($queryProject->getResult());
+    return $dependencies != 0;
+  }
 
-        $editForm = $this->createForm(new LocationType(), $entity);
-        $deleteForm = $this->createDeleteForm($id);
-
-        $request = $this->getRequest();
-
-        $editForm->bindRequest($request);
-
-        if ($editForm->isValid()) {
-            $em->persist($entity);
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('admin_location_edit', array('id' => $id)));
-        }
-
-        return array('entity' => $entity, 'edit_form' => $editForm->createView(), 'delete_form' => $deleteForm->createView(),);
+  /**
+   * Deletes a Location entity.
+   *
+   * @Route("/{id}/delete", name="admin_location_delete")
+   * @Method("post")
+   */
+  public function deleteAction($id)
+  {
+    if ($this->hasDependencies($id)) {
+      return $this->redirect($this->generateUrl('admin_location_edit', array('id' => $id)));
     }
 
-    /**
-     * Deletes a Location entity.
-     *
-     * @Route("/{id}/delete", name="admin_location_delete")
-     * @Method("post")
-     */
-    public function deleteAction($id)
-    {
-        $form = $this->createDeleteForm($id);
-        $request = $this->getRequest();
+    $form = $this->createDeleteForm($id);
+    $request = $this->getRequest();
 
-        $form->bindRequest($request);
+    $form->bindRequest($request);
 
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getEntityManager();
-            $entity = $em->getRepository('OGInversaBundle:Location')->find($id);
+    if ($form->isValid()) {
+      $em = $this->getDoctrine()->getEntityManager();
+      $entity = $em->getRepository('OGInversaBundle:Location')->find($id);
 
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find Location entity.');
-            }
+      if (!$entity) {
+        throw $this->createNotFoundException('Unable to find Location entity.');
+      }
 
-            $em->remove($entity);
-            $em->flush();
-        }
-
-        return $this->redirect($this->generateUrl('admin_location'));
+      $em->remove($entity);
+      $em->flush();
     }
 
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder(array('id' => $id))->add('id', 'hidden')->getForm();
-    }
+    return $this->redirect($this->generateUrl('admin_location'));
+  }
+
+  private function createDeleteForm($id)
+  {
+    return $this->createFormBuilder(array('id' => $id))->add('id', 'hidden')->getForm();
+  }
 }
